bfedea3328a9eb72d96345f5bd564423d71d35e5
sapho-sys <saphonkunzi@gmail.com>
2022-11-08 18:22:40 +0200

    progrss

--- services/data-factory.js
+++ services/data-factory.js
@@ -1,10 +1,30 @@
 function DataFactory(db){
     async function getAll(){
-        const fethData = await db.manyOrNone(`SELECT * FROM shoes`);
+        const fethData = await db.manyOrNone(`SELECT * FROM shoes WHERE in_stock > 0`);
         return fethData;
     }
+    async function getSize(size){
+        const sizeData = await db.manyOrNone(`SELECT * FROM shoes WHERE 
+        size = $1 AND in_stock > 0`,[size]);
+        return sizeData;
+    }
+
+    async function getBrand(brand){
+        const brandData = await db.manyOrNone(`SELECT * FROM shoes WHERE
+         brand = $1 AND in_stock > 0`,[brand]);
+         return brandData;
+    }
+    
+    async function getColor(color){
+        const colorData = await db.manyOrNone(`SELECT * FROM shoes WHERE
+        brand = $1 AND in_stock > 0`,[color]);
+        return colorData;
+    }
     return{
-        getAll
+        getAll,
+        getSize,
+        getBrand
+
     }
 
 }
